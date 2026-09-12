3f0aef0690312206374e86ede0e6efa40f4c73d0
Ethan <your.email@example.com>
2026-09-12 10:21:46 -0500

    feat(developer-registration): add registration form.

--- src/greetings/domain/model/developer.entity.js
+++ src/greetings/domain/model/developer.entity.js
@@ -1,4 +1,4 @@
-import {PersonName} from "../../../shared/domain/model/person-name.value-object.js";
+import {PersonName} from "../../../shared/domain/person-name.value-object.js";
 import {DeveloperId} from "./developer-id.value-object.js";
 
 export class Developer {
